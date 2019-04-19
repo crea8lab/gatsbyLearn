f0a3599174347a5022300244551ac4db55c647f1
Fayvor George <phavorsparks@gmail.com>
2019-04-19 07:30:11 +0100

    Styled the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,26 +6,54 @@ import headerStyles from "../styles/components/header.module.scss"
 
 const Header = () => {
   return (
-    <div>
-      <div className="logo">gatsbyLearnr</div>
+    <header className={headerStyles.header}>
+      <h1 className="logo">
+        <Link to="/" className={headerStyles.title}>
+          Gatsby<span className={headerStyles.title__span}>Learn</span>
+        </Link>
+      </h1>
 
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/"
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/blog"
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/docs">Docs</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/docs"
+            >
+              Docs
+            </Link>
           </li>
           <li>
-            <Link to="/tutorials">Tutorials</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/tutorials"
+            >
+              Tutorials
+            </Link>
           </li>
         </ul>
       </nav>
-    </div>
+    </header>
   )
 }
 
